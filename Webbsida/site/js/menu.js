a1e8e907645da6473cbd9f3a1f2b6e59ae3c0a02
itggot-jonas-nordin <jonas.nordin@itggot.se>
2018-05-26 15:26:36 +0200

    webbsite done

--- Webbsida/site/js/menu.js
+++ Webbsida/site/js/menu.js
@@ -1,9 +1,9 @@
 function toggleMenu(){
     var element = document.querySelector(".menu");
     element.classList.toggle("show-menu");
+}
 
-/*
-    element = document.querySelector("main");
-    element.classList.toggle("fade-main");
-*/
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
 }
