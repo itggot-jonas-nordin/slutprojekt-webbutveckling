cff9e97fbc2ad90087e110d54b1a5636713474ea
itggot-jonas-nordin <jonas.nordin@itggot.se>
2018-05-27 13:45:14 +0200

    minify.rb tillagd och använd

--- Webbsida/site/public/js/menu.js
+++ Webbsida/site/public/js/menu.js
@@ -2,8 +2,3 @@ function toggleMenu(){
     var element = document.querySelector(".menu");
     element.classList.toggle("show-menu");
 }
-
-function openInNewTab(url) {
-    var win = window.open(url, '_blank');
-    win.focus();
-}
